9a3422e4ee225551e1a980a94c970528807b0a44
Morgan Lieberthal <morgan@jmorgan.org>
2016-10-12 20:20:29 -0600

    Add more wrappers around Variadics

--- Sources/YajlConfig/YajlConfig.c
+++ Sources/YajlConfig/YajlConfig.c
@@ -1,7 +1,23 @@
 #include "YajlConfig.h"
 
-int yajl_handle_config_int(yajl_handle h, yajl_option opt, int value) {
-  if (yajl_config(h, opt, value)) {
+int yajl_handle_config_int(yajl_handle h, yajl_option opt, size_t value) {
+  if (yajl_config(h, opt, (int)value)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+int yajl_gen_config_int(yajl_gen gen, yajl_gen_option opt, size_t value) {
+  if (yajl_gen_config(gen, opt, (int)value)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+int yajl_gen_config_string(yajl_gen g, yajl_gen_option opt, const char *value) {
+  if (yajl_gen_config(g, opt, value)) {
     return 1;
   }
 
--- Sources/YajlConfig/include/YajlConfig.h
+++ Sources/YajlConfig/include/YajlConfig.h
@@ -2,11 +2,22 @@
 #define SWIFT_YAJL_CONFIG_H 1
 
 #include <CoreFoundation/CoreFoundation.h>
+#include <yajl/yajl_gen.h>
 #include <yajl/yajl_parse.h>
 
 // clang-format off
 CF_SWIFT_NAME(configureYajlHandle(_:option:intValue:))
 // clang-format on
-extern int yajl_handle_config_int(yajl_handle h, yajl_option opt, int value);
+extern int yajl_handle_config_int(yajl_handle h, yajl_option opt, size_t value);
+
+// clang-format off
+CF_SWIFT_NAME(configureYajlGenerator(_:option:intValue:))
+// clang-format on
+extern int yajl_gen_config_int(yajl_gen gen, yajl_gen_option opt, size_t value);
+
+// clang-format off
+CF_SWIFT_NAME(configureYajlGenerator(_:option:stringValue:))
+// clang-format on
+extern int yajl_gen_config_string(yajl_gen, yajl_gen_option, const char *);
 
 #endif /* ifndef SWIFT_YAJL_CONFIG_H */

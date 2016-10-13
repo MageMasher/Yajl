0024785faa991b90b25f2d69ee0de9b004c8d4fc
Morgan Lieberthal <morgan@jmorgan.org>
2016-10-12 19:56:41 -0600

    Add more options for the parser.

--- Sources/Yajl/ParserOptions.swift
+++ Sources/Yajl/ParserOptions.swift
@@ -23,9 +23,30 @@ extension YajlParser {
     
     /// Invalid UTF8 strings will cause the parser to throw an error.
     public static let validateUTF8    = Options(rawValue: 1 << 1)
-    
-    /// Will force strict precision on numbers and the parser will throw a 
-    /// `ParserError.integerOverflow` if integer overflow is detected. 
-    public static let strictPrecision = Options(rawValue: 1 << 2)
+
+    /// By default, upon calls to yajl_complete_parse(), yajl will ensure the 
+    /// entire input text was consumed and will raise an error otherwise. 
+    ///
+    /// Enabling this flag will cause yajl to disable this check. This can be 
+    /// useful when parsing json out of a that contains more than a single JSON
+    /// document.
+    public static let allowTrailing   = Options(rawValue: 1 << 2)
+
+    /// Allow multiple values to be parsed by a single handle. The entire text
+    /// must be valid JSON, and values can be seperated by any kind of
+    /// whitespace.
+    ///
+    /// This flag will change the behavior of the parser, and cause it continue 
+    /// parsing after a value is parsed, rather than transitioning into a
+    /// complete state. This option can be useful when parsing multiple values
+    /// from an input stream.
+    public static let allowMultiple   = Options(rawValue: 1 << 3)
+
+    /// When yajl_complete_parse() is called the parser will check that the top 
+    /// level value was completely consumed. I.E., if called whilst in the middle 
+    /// of parsing a value yajl will enter an error state (premature EOF). 
+    ///
+    /// Setting this flag suppresses that check and the corresponding error.
+    public static let allowPartial    = Options(rawValue: 1 << 4)
   }
 }

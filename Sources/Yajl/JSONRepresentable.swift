95f0d6dbf6dc6c3002381020e7d3ff7c927fedbb
Morgan Lieberthal <morgan@jmorgan.org>
2016-10-12 19:56:23 -0600

    Refactor JSONRepresentable extensions. Add extensions to Bool, Int, Double, String, Array, and Dictionary to conform to JSONSerializable.

--- Sources/Yajl/JSONRepresentable.swift
+++ Sources/Yajl/JSONRepresentable.swift
@@ -5,8 +5,6 @@
 //  Created by J. Morgan Lieberthal on 10/11/15
 //
 
-import CYajl
-
 /// Wraps a JSON-representable type
 public enum JSONRepresentable {
   /// The `null` value
@@ -20,7 +18,7 @@ public enum JSONRepresentable {
   /// While not strictly present in JSON (all javascript numbers are doubles),
   /// this is convenient
   case int(Int)
-  
+
   /// A `float`
   case double(Double)
 
@@ -50,23 +48,13 @@ extension JSONRepresentable: CustomStringConvertible {
   }
 }
 
-// MARK: - ExpressibleByNilLiteral
+// MARK: - ExpressibleBy...
 
-extension JSONRepresentable: ExpressibleByNilLiteral {
-  public init(nilLiteral: ()) {
-    self = .null
+extension JSONRepresentable: ExpressibleByBooleanLiteral {
+  public init(booleanLiteral value: Bool) {
+    self = .bool(value)
   }
 }
-
-// MARK: - ExpressibleByArrayLiteral
-
-extension JSONRepresentable: ExpressibleByArrayLiteral {
-  public init(arrayLiteral elements: JSONRepresentable...) {
-    self = .array(elements)
-  }
-}
-
-// MARK: - ExpressibleByIntegerLiteral 
 
 extension JSONRepresentable: ExpressibleByIntegerLiteral {
   public init(integerLiteral value: Int) {
@@ -74,15 +62,11 @@ extension JSONRepresentable: ExpressibleByIntegerLiteral {
   }
 }
 
-// MARK: - ExpressibleByFloatLiteral
-
 extension JSONRepresentable: ExpressibleByFloatLiteral {
   public init(floatLiteral value: Double) {
     self = .double(value)
   }
 }
-
-// MARK: - ExpressibleByStringLiteral
 
 extension JSONRepresentable: ExpressibleByStringLiteral {
   public init(stringLiteral value: String) {
@@ -98,25 +82,21 @@ extension JSONRepresentable: ExpressibleByStringLiteral {
   }
 }
 
-// MARK: - ExpressibleByDictionaryLiteral
-
-extension JSONRepresentable: ExpressibleByDictionaryLiteral {
-  public init(dictionaryLiteral elements: (String, JSONRepresentable)...) {
-    var theDict: [String: JSONRepresentable] = [:]
-
-    for (key, value) in elements {
-      theDict[key] = value
-    }
-    
-    self = .dict(theDict)
+extension JSONRepresentable: ExpressibleByArrayLiteral {
+  public init(arrayLiteral elements: JSONSerializable...) {
+    self = .array(elements.map({ $0.toJSON() }))
   }
 }
 
-// MARK: - ExpressibleByBooleanLiteral
+extension JSONRepresentable: ExpressibleByDictionaryLiteral {
+  public init(dictionaryLiteral elements: (String, JSONSerializable)...) {
+    var theDict: [String: JSONRepresentable] = [:]
 
-extension JSONRepresentable: ExpressibleByBooleanLiteral {
-  public init(booleanLiteral value: Bool) {
-    self = .bool(value)
+    for (key, value) in elements {
+      theDict[key] = value.toJSON()
+    }
+
+    self = .dict(theDict)
   }
 }
 
@@ -159,15 +139,10 @@ private let nsBooleanType = type(of: NSNumber(value: false))
 
 extension JSONRepresentable {
   /// Tries to convert `Any` to `JSONRepresentable`, returning `.null` if conversion failed.
-  static func convertToJSON(_ object: Any) -> JSONRepresentable! {
-    switch object {
-    case is NSNull:
-      return .null
+  static func convertToJSON(_ object: Any?) -> JSONRepresentable! {
+    guard let object = object else { return .null }
 
-    case let value as String:
-      return .string(value)
-
-    case let value as NSNumber:
+    func nsNumberToJSON(_ value: NSNumber) -> JSONRepresentable {
       // Check if this is a boolean NSNumber
       // FIXME: This is all rather expensive...
       if type(of: value) === nsBooleanType {
@@ -180,34 +155,38 @@ extension JSONRepresentable {
       }
 
       return .double(value.doubleValue)
+    }
 
-    case let value as NSArray:
-      return .array(value.map(convertToJSON))
-
-    case let value as NSDictionary:
+    func nsDictionaryToJSON(_ value: NSDictionary) -> JSONRepresentable {
       var result = [String: JSONRepresentable]()
       value.forEach({ result[$0 as! String] = convertToJSON($1) })
       return .dict(result)
+    }
 
-    case let asBool as Bool:
-      return .bool(asBool)
-
-    case let asInt as Int:
-      return .int(asInt)
-
-    case let asDouble as Double:
-      return .double(asDouble)
-
-    case let value as [Any]:
-      return .array(value.map(convertToJSON))
-
-    case let value as [String: Any]:
+    func stringAnyToJSON(_ value: [String: Any]) -> JSONRepresentable {
       var result = [String: JSONRepresentable]()
       value.forEach({ result[$0] = convertToJSON($1) })
       return .dict(result)
+    }
 
-    default:
-      return nil
+    switch object {
+    case is NSNull: return .null
+
+    case let value as JSONRepresentable: return value
+    case let value as JSONSerializable:  return value.toJSON()
+    case let value as String:            return .string(value)
+    case let value as NSNumber:          return nsNumberToJSON(value)
+    case let value as NSArray:           return .array(value.map(convertToJSON))
+    case let value as NSDictionary:      return nsDictionaryToJSON(value)
+
+    case let value as Bool:   return .bool(value)   // `true` on Linux
+    case let value as Int:    return .int(value)    // `true` on Linux
+    case let value as Double: return .double(value) // `true` on Linux
+
+    case let value as [Any]:         return .array(value.map(convertToJSON))
+    case let value as [String: Any]: return stringAnyToJSON(value)
+
+    default: return nil
     }
   }
 
@@ -226,7 +205,59 @@ extension JSONRepresentable {
 /// Defines an interface for types that can be serialized in JSON format.
 public protocol JSONSerializable {
   /// Converts a custom object to `JSONRepresentable`
-  /// 
+  ///
   /// - returns: `JSONRepresentable` of the custom object.
   func toJSON() -> JSONRepresentable
+}
+
+extension JSONRepresentable: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    return self
+  }
+}
+
+// MARK: - JSONSerializable for Standard Library Types
+
+extension Bool: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    return .bool(self)
+  }
+}
+
+extension Int: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    return .int(self)
+  }
+}
+
+extension Double: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    return .double(self)
+  }
+}
+
+extension String: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    return .string(self)
+  }
+}
+
+extension Array: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    if let json = JSONRepresentable.convertToJSON(self) {
+      return json
+    }
+
+    return .array([])
+  }
+}
+
+extension Dictionary: JSONSerializable {
+  public func toJSON() -> JSONRepresentable {
+    if let json = JSONRepresentable.convertToJSON(self) {
+      return json
+    }
+
+    return .dict([:])
+  }
 }
